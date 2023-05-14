721ef6de37b190dce005befc19c0114abb338b64
vonvolous <hanyoojin39@gmail.com>
2023-05-15 06:59:54 +0900

    edit landing page

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,1 +1,3 @@
-export const API
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = '147e8bf6fcd6490d72c72402c768e876';
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
